609bac246743fdc68926ad813eb8b7d2a1b1d2ac
BohdanMatiushko <zerox2007ss@gmail.com>
2026-02-26 14:51:39 +0200

    delete develop price

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-#prices developer bogdan = 100000000$
-#prices developer 1 = 100000$
-#prices developer 2 = 1000$
+
 
 @app.get("/")
 async def root():
