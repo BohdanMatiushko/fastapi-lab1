4467684b8f4172bd8f32c7dacc86916432347173
BohdanMatiushko <zerox2007ss@gmail.com>
2026-03-11 20:49:56 +0200

    add database session dependency for fastapi endpoints

---
+++ app/api/deps.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from typing import AsyncGenerator
+
+from app.db.session import AsyncSessionLocal
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session
