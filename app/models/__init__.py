fbc76b215a758e2cd94203cab629e3628bbd163f
BohdanMatiushko <zerox2007ss@gmail.com>
2026-03-11 21:06:59 +0200

    export all models in models/__init__.py for easier imports

---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+from app.models.user import User
+from app.models.profile import Profile
+from app.models.category import Category
+from app.models.product import Product
+from app.models.order import Order
+from app.models.order_item import OrderItem
+
+__all__ = ["User", "Profile", "Category", "Product", "Order", "OrderItem"]
